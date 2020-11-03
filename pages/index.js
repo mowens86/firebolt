f4bdc508f47efb241c322d75cd119ff4784bc0d3
Mike Owens <mocoding86@gmail.com>
2020-11-02 21:14:13 -0800

    decided to use deezer api for project and add links to footer

--- pages/index.js
+++ pages/index.js
@@ -16,19 +16,32 @@ export default function Home() {
           </div>
         </section>
 
+        <section>
+          <div>
+
+          </div>
+        </section>
+
       </main>
 
       <footer className={styles.footer}>
         <a
-          className={styles.hover}
+          className={styles.footerLink}
           href="https://www.deezer.com/us/"
           target="_blank"
           rel="noopener noreferrer"
         >
           <img src="/firebolt.svg" alt="Firebolt Logo" className={styles.logo} />
-          {' '} Music data powered by XXX
+          {' '} Music data powered by:
           
         </a>
+          <a 
+          href="/deezer.png"
+          href="https://www.deezer.com/us/"
+          target="_blank"
+          >
+            <img src="/deezer.png" alt="Firebolt Logo" className={styles.deezer} />
+          </a>
       </footer>
     </div>
   )
