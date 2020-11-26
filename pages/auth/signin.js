2d48bbe5bd652c621c8ab584e591455363e971b6
Mike Owens <mocoding86@gmail.com>
2020-11-26 07:36:49 -0800

    finish signin page styling

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -6,19 +6,31 @@ import { providers, signIn } from 'next-auth/client';
 export default function SignIn({ providers }) {
   return (
     <Layout>
+      
+        <div className={styles.signInWrapper}>
+        <div className={styles.overlay}>
+          <div className={styles.logoWrapper}>
+            <img src="/firebolt.svg" alt="Firebolt Logo" className={styles.logo} />
+            <h2 className={styles.title}>Firebolt</h2>
+          </div>
+     
       {Object.values(providers).map(provider => (
-        <div >
+
+
           <div 
               className={styles.linkWrapper} 
               key={provider.name}>
             <a 
               className={styles.link} 
-              onClick={() => signIn(provider.id, { callbackUrl: 'http://localhost:3000/' })}
+              onClick={() => signIn(provider.id, { callbackUrl: 'http://localhost:3000/dashboard' })}
               >Sign in with {provider.name}
             </a>
+
           </div>
-        </div>
+        
       ))}
+        </div>
+      </div>
     </Layout>
   )
 }
